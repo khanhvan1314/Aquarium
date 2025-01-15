db1bbe869d733b05fc257cecfece2b347e358e7b
khanhvan1314 <trankhanhvan.1922@gmail.com>
2025-01-15 12:59:18 +0700

    i

--- deploy.py
+++ deploy.py
@@ -153,4 +153,4 @@ async def predict_image(file: UploadFile = File(...)):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=8001)
